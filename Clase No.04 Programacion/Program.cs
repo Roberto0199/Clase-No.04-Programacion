391799d8fc62db5e1ffea86185356c9c2946fec6
rober <rober@DESKTOP-F30IEGM>
2023-02-25 10:13:51 -0600

    Agregar archivos de proyecto.

---
+++ Clase No.04 Programacion/Program.cs
@@ -0,0 +1,49 @@
+﻿/*int x = 1;
+while (x <= 500)
+{
+    Console.WriteLine(x);
+
+    x++;
+
+}
+Console.WriteLine("Fin!!");*/
+
+/*int x = 50;
+while (x <= 100)
+{
+    Console.WriteLine(x);
+
+    x++;
+
+}
+Console.WriteLine("Fin!!");*/
+
+/*int x = -50;
+while (x <= 0)
+{
+    Console.WriteLine(x);
+
+    x++;
+
+}
+Console.WriteLine("Fin!!");*/
+
+
+
+
+
+
+
+int x = 2;
+while(x<=100)
+{
+    Console.WriteLine(x);
+
+    x=x+2;
+  
+}
+Console.WriteLine("Fin!!");
+
+
+
+
